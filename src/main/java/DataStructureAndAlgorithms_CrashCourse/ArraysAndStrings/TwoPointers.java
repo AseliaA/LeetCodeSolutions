8ad9c9b5d2eaa62e7c6f2271e9f3fb8a256f3812
AsiA203 <aselia.azimkanova@gmail.com>
2024-11-20 22:55:38 -0600

    Implemented SortedSquareArray function with "Two Pointers" method

--- src/main/java/DataStructureAndAlgorithms_CrashCourse/ArraysAndStrings/TwoPointers.java
+++ src/main/java/DataStructureAndAlgorithms_CrashCourse/ArraysAndStrings/TwoPointers.java
@@ -1,12 +1,26 @@
 package DataStructureAndAlgorithms_CrashCourse.ArraysAndStrings;
 
+import java.util.Arrays;
+
 public class TwoPointers {
     public static void main(String[] args) {
         //Check if Palindrome with "Two Pointers" method
         System.out.println(checkIfPalindrome("abcbca"));
 
         //Reverse String with "Two Pointers" method
-        System.out.println(reverseString(new char[] {'h','e','l','l','o'}));
+        System.out.println(reverseString(new char[]{'h', 'e', 'l', 'l', 'o'}));
+
+        /*
+        Given an integer array nums sorted in non-decreasing order,
+        return an array of the squares of each number sorted in non-decreasing order.
+
+        Input: nums = [-4,-1,0,3,10]
+        Output: [0,1,9,16,100]
+        Explanation: After squaring, the array becomes [16,1,0,9,100].
+        After sorting, it becomes [0,1,9,16,100].
+        */
+        int [] newNumArray = sortedSquares(new int[]{-4, -1, 0, 3, 10});
+        System.out.println(Arrays.toString(newNumArray));
     }
 
     public static boolean checkIfPalindrome(String string) {
@@ -24,12 +38,12 @@ public class TwoPointers {
         return true;
     }
 
-    public static char[] reverseString(char [] s){
+    public static char[] reverseString(char[] s) {
         //without edge cases
         int left = 0;
         int right = s.length - 1;
 
-        while (left < right){
+        while (left < right) {
             char tem = s[left];
             s[left] = s[right];
             s[right] = tem;
@@ -38,5 +52,26 @@ public class TwoPointers {
             right--;
         }
         return s;
+    }
+
+    public static int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int square;
+            if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+                square = nums[right];
+                right--;
+            } else {
+                square = nums[left];
+                left++;
+            }
+            result[i] = square * square;
+        }
+        return result;
     }
 }
