a98e6442c743218831a89028183a4557963131db
AsiA203 <aselia.azimkanova@gmail.com>
2024-11-20 22:17:39 -0600

    Implemented ReverseString function with "Two Pointers" method

--- src/main/java/DataStructureAndAlgorithms_CrashCourse/ArraysAndStrings/TwoPointers.java
+++ src/main/java/DataStructureAndAlgorithms_CrashCourse/ArraysAndStrings/TwoPointers.java
@@ -4,20 +4,39 @@ public class TwoPointers {
     public static void main(String[] args) {
         //Check if Palindrome with "Two Pointers" method
         System.out.println(checkIfPalindrome("abcbca"));
+
+        //Reverse String with "Two Pointers" method
+        System.out.println(reverseString(new char[] {'h','e','l','l','o'}));
     }
 
-    public static boolean checkIfPalindrome(String array) {
+    public static boolean checkIfPalindrome(String string) {
         //without edge cases
         int left = 0;
-        int right = array.length() - 1;
+        int right = string.length() - 1;
 
         while (left < right) {
-            if (array.charAt(left) != array.charAt(right)) {
+            if (string.charAt(left) != string.charAt(right)) {
                 return false;
             }
             left++;
             right--;
         }
         return true;
+    }
+
+    public static char[] reverseString(char [] s){
+        //without edge cases
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right){
+            char tem = s[left];
+            s[left] = s[right];
+            s[right] = tem;
+
+            left++;
+            right--;
+        }
+        return s;
     }
 }
