cbda87f90ae45022e42374a174914c166a85a025
AsiA203 <aselia.azimkanova@gmail.com>
2025-01-17 19:55:46 -0600

    - Solved and also added Azret's solution for SnakeInMatrix_3248.java - Also added Azret's ver2 solution for ReverseString_344.java

--- src/main/java/MathAlgo_withAzret/Easy/ReverseString_344.java
+++ src/main/java/MathAlgo_withAzret/Easy/ReverseString_344.java
@@ -9,6 +9,26 @@ public class ReverseString_344 {
         System.out.println(reverseString(c));
     }
 
+    //v2 - Azret's version
+    public static char [] reverseString_azretVer2(char [] s) {
+        int i = 0;
+        int j = s.length - 1;
+        while ( i < j) {
+            char temp = s[j];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+        return s;
+    }
+
+    //v1 - Azret's version
+    public static char [] reverseString_azretVer1(char [] s) {
+        char [] t = new StringBuilder().append(s).reverse().toString().toCharArray();
+        System.arraycopy(t, 0, s, 0, s.length);
+        return null;
+    }
     //v1 - Runtime: 0ms, beats 100% | Memory: 49.24mb, beats 43.28%
     //Time Complexity: O(n)
     //Memory Complexity: 0(1)
--- src/main/java/MathAlgo_withAzret/Easy/SnakeInMatrix_3248.java
+++ src/main/java/MathAlgo_withAzret/Easy/SnakeInMatrix_3248.java
@@ -2,6 +2,61 @@ package MathAlgo_withAzret.Easy;
 
 public class SnakeInMatrix_3248 {
     public static void main(String [] args) {
+        System.out.println(snakeInMatrix_AzretVer2(3, new String []{"RIGHT", "RIGHT", "DOWN", "LEFT"}));
+        System.out.println(snakeInMatrix_AzretVer1(3, new String []{"RIGHT", "RIGHT", "DOWN", "LEFT"}));
+        System.out.println(snakeInMatrix(3, new String []{"RIGHT", "RIGHT", "DOWN", "LEFT"}));
+    }
 
+    //Azret ver2
+    public static int snakeInMatrix_AzretVer2(int n, String [] commands) {
+        int number = 0;
+        for(String command : commands) {
+            if(command.equals("UP")) {
+                number -= n;
+            } else if(command.equals("DOWN")) {
+                number += n;
+            } else if(command.equals("RIGHT")) {
+                number++;
+            } else if(command.equals("LEFT")) {
+                number--;
+            }
+        }
+        return number;
+    }
+
+    //Azret ver1
+    public static int snakeInMatrix_AzretVer1(int n, String [] commands) {
+        int row = 0;
+        int col = 0;
+
+        for(String command : commands) {
+           if(command.equals("UP")) {
+               row--;
+           } else if(command.equals("DOWN")) {
+               row++;
+           } else if(command.equals("RIGHT")) {
+               col++;
+           } else if(command.equals("LEFT")) {
+               col--;
+           }
+        }
+        return row * n + col;
+    }
+
+
+    //my solution after watching lesson recoding and kinda looking at the solution
+    public static int snakeInMatrix(int n, String [] commands) {
+        int row = 0;
+        int col = 0;
+
+        for(String command : commands) {
+            switch(command) {
+                case "UP": row--; break;
+                case "DOWN": row++; break;
+                case "RIGHT": col++; break;
+                case "LEFT": col--; break;
+            }
+        }
+        return row * n + col;
     }
 }
