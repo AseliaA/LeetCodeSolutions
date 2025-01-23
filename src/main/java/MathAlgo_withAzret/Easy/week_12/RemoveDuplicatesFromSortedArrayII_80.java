c4ad8846d4fdf01fcf21ec86919d2da752ec05d2
AsiA203 <aselia.azimkanova@gmail.com>
2025-01-22 19:44:43 -0600

    - Watched lesson recording for RemoveDuplicatesFromSortedArrayII_80.java

---
+++ src/main/java/MathAlgo_withAzret/Easy/week_12/RemoveDuplicatesFromSortedArrayII_80.java
@@ -0,0 +1,20 @@
+package MathAlgo_withAzret.Easy.week_12;
+
+import java.util.Arrays;
+
+public class RemoveDuplicatesFromSortedArrayII_80 {
+    public static void main(String [] args) {
+        System.out.println(removeDuplicates(new int[]{1, 1, 2, 2, 3, 3}));
+
+    }
+
+    public static int removeDuplicates(int [] nums) {
+        int insertIndex = 2;
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[insertIndex - 2]) {
+                nums[insertIndex++] = nums[i];
+            }
+        }
+        return insertIndex;
+    }
+}
