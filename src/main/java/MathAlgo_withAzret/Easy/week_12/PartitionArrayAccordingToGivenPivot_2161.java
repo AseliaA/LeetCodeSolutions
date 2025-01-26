0bafa937f762d27644c32c2d0d868ea6d91ab456
AsiA203 <aselia.azimkanova@gmail.com>
2025-01-25 21:32:16 -0600

    - Watched solution recording for PartitionArrayAccordingToGivenPivor_2161.java

--- src/main/java/MathAlgo_withAzret/Easy/week_12/PartitionArrayAccordingToGivenPivot_2161.java
+++ src/main/java/MathAlgo_withAzret/Easy/week_12/PartitionArrayAccordingToGivenPivot_2161.java
@@ -1,9 +1,41 @@
 package MathAlgo_withAzret.Easy.week_12;
 
+import java.util.Arrays;
+
 public class PartitionArrayAccordingToGivenPivot_2161 {
     public static void main(String [] args) {
-
+        System.out.println(Arrays.toString(pivotArray_ver1Azret(new int[]{9,12,5,10,14,3,10}, 10)));
     }
+    //Ver1 from Azret
+    //Quick-Sort Algorithm
+    //Time complexity: O(3n) -> O(n)
+    //Memory complexity: O(n) + O(1) -> O(n)
+    public static int [] pivotArray_ver1Azret(int [] nums, int pivot) {
+        int [] result = new int[nums.length];
+        int insertPosition = 0;
+        int pivotCounter = 0;
+
+        for (int num : nums) {
+            if (num < pivot) {
+                result[insertPosition++] = num;
+            } else if (num == pivot) {
+                pivotCounter++;
+            }
+        }
+
+        while (pivotCounter > 0) {
+            result[insertPosition++] = pivot;
+            pivotCounter--;
+        }
+
+        for (int num : nums) {
+            if (num > pivot) {
+                result[insertPosition++] = num;
+            }
+        }
+        return result;
+    }
+
     //Overall Time by myself: 20min for all version//
     //Time Limit Exceeded
     public static int[] pivotArray_ver2Me(int [] nums, int pivot) {
