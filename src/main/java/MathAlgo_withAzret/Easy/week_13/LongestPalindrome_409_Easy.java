7f2ceaac82b6084b9c233400d0a9f0fbfb33222d
AsiA203 <aselia.azimkanova@gmail.com>
2025-01-28 20:32:40 -0600

    - Watched recording and completed LongestPalindrome_409_Easy.java

---
+++ src/main/java/MathAlgo_withAzret/Easy/week_13/LongestPalindrome_409_Easy.java
@@ -0,0 +1,33 @@
+package MathAlgo_withAzret.Easy.week_13;
+
+import java.util.HashSet;
+
+public class LongestPalindrome_409_Easy {
+    public static void main(String [] args) {
+        System.out.println(longestPalindrome("abccccdd"));
+        System.out.println(longestPalindrome("aaa"));
+        System.out.println(longestPalindrome("b"));
+    }
+
+    public static int longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        HashSet<Character> hashSet = new HashSet<>();
+        int palindromeLength = 0;
+        for (char ch : s.toCharArray()) {
+            if (hashSet.contains(ch)) {
+                hashSet.remove(ch);
+                palindromeLength += 2;
+            } else {
+                hashSet.add(ch);
+            }
+        }
+
+        if (!hashSet.isEmpty()) {
+            palindromeLength++;
+        }
+        return palindromeLength;
+    }
+}
