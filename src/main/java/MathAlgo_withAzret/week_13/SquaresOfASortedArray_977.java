5ea6ef4298be3c1b0c1f31c9b355892e67a247d6
AsiA203 <aselia.azimkanova@gmail.com>
2025-01-29 21:33:47 -0600

    - Solved SquaresOfASortedArray_977.java

--- src/main/java/MathAlgo_withAzret/week_13/SquaresOfASortedArray_977.java
+++ src/main/java/MathAlgo_withAzret/week_13/SquaresOfASortedArray_977.java
@@ -1,7 +1,34 @@
 package MathAlgo_withAzret.week_13;
 
+import java.util.Arrays;
+
 public class SquaresOfASortedArray_977 {
     public static void main(String [] args) {
+        System.out.println(Arrays.toString(sortedSquares_ver1(new int[]{-4,-1,0,3,10})));
+    }
 
+    //my ver1
+    //Time Com: O(n)
+    //Memory Com: O(n)
+    public static int[] sortedSquares_ver1(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return new int[]{};
+        }
+
+        int [] result = new int[nums.length];
+        int left = 0;
+        int right = nums.length - 1;
+        for (int i = result.length - 1; i >= 0; i--) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+            if (leftSquare > rightSquare) {
+                result[i] = leftSquare;
+                left++;
+            } else {
+                result[i] = rightSquare;
+                right--;
+            }
+        }
+        return result;
     }
 }
