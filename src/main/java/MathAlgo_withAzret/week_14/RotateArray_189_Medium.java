227749791ff0e9ecb2674b503f23985d902adf77
AsiA203 <aselia.azimkanova@gmail.com>
2025-02-05 19:45:33 -0600

    - Added brute force solution for RotateArray_189_Medium.java

--- src/main/java/MathAlgo_withAzret/week_14/RotateArray_189_Medium.java
+++ src/main/java/MathAlgo_withAzret/week_14/RotateArray_189_Medium.java
@@ -1,7 +1,34 @@
 package MathAlgo_withAzret.week_14;
 
+import java.util.Arrays;
+
 public class RotateArray_189_Medium {
     public static void main(String [] args) {
+        System.out.println("Brute Force: " + Arrays.toString(rotateArray_bruteForceVer(new int[]{1,2,3,4,5,6,7}, 3)));
+    }
 
+    //Brute Force version
+    //Time Comp: O(n)
+    //Memory Comp: O(n)
+    public static int[] rotateArray_bruteForceVer(int[] nums, int k) {
+        int [] res = new int [nums.length];
+        int leftNums = nums.length - 1;
+        int resPointer = 0;
+        int rotatePointer = k - 1;
+
+        while (rotatePointer >= 0) {
+            res[rotatePointer--] = nums[leftNums--];
+            resPointer++;
+        }
+
+        for(int i = 0; i < nums.length - k; i++) {
+            res[resPointer++] = nums[i];
+        }
+
+        System.arraycopy(res, 0, nums, 0, nums.length);
+//        for(int i = 0; i < nums.length; i++) {
+//            nums[i] = res[i];
+//        }
+        return nums;
     }
 }
