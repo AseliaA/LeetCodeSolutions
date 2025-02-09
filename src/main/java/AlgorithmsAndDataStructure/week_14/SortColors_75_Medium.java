4fa20dce062317252aa61047c8ef01434ec84347
AsiA203 <aselia.azimkanova@gmail.com>
2025-02-08 20:52:56 -0600

    Sort Color_75_Medium: - Watched lesson recording and implemented Dutch Flag Algorithm

--- src/main/java/AlgorithmsAndDataStructure/week_14/SortColors_75_Medium.java
+++ src/main/java/AlgorithmsAndDataStructure/week_14/SortColors_75_Medium.java
@@ -1,8 +1,32 @@
 package AlgorithmsAndDataStructure.week_14;
 
+import java.util.Arrays;
+
 public class SortColors_75_Medium {
     public static void main(String[] args) {
+        System.out.println("Ver1: " + Arrays.toString(sortColor_ver1(new int[]{1,0,2,2,1,0})));
+        System.out.println("Dutch Flag Algo: " + Arrays.toString(sortColor_DutchFlagAlgo(new int[]{1,0,2,2,1,0})));
+    }
 
+    //Dutch Flag Algo
+    //Time Comp: O(n)
+    //Memory Comp: O(1)
+    //Probably most optimal
+    public static int[] sortColor_DutchFlagAlgo(int [] nums) {
+        int zerosInsertPosition = 0;
+        int twosInsertPosition = nums.length - 1;
+        int i = 0;
+
+        while (i < twosInsertPosition) {
+            if (nums[i] == 0) {
+                swap(nums, zerosInsertPosition++, i++);
+            } else if (nums[i] == 1) {
+                i++;
+            } else {
+                swap(nums, twosInsertPosition--, i);
+            }
+        }
+        return nums;
     }
 
     //Could remember solution from week_13 and implement it
@@ -22,6 +46,7 @@ public class SortColors_75_Medium {
         return nums;
     }
 
+    //helper function
     public static void swap(int[] nums, int x, int y) {
         int temp = nums[x];
         nums[x] = nums[y];
