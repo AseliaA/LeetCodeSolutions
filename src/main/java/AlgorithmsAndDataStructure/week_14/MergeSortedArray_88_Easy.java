2fb5c536a6993188a0ed8e45b347aa45a74906b5
AsiA203 <aselia.azimkanova@gmail.com>
2025-02-09 22:31:49 -0600

    - Solved with two pointers MergeSortedArray_88_Easy.java

--- src/main/java/AlgorithmsAndDataStructure/week_14/MergeSortedArray_88_Easy.java
+++ src/main/java/AlgorithmsAndDataStructure/week_14/MergeSortedArray_88_Easy.java
@@ -5,7 +5,18 @@ public class MergeSortedArray_88_Easy {
 
     }
 
-    public static int[] merge_ver1(int[] num1, int m, int[] nums2, int n) {
+    public static int[] merge_ver1(int[] nums1, int m, int[] nums2, int n) {
+        int nums1End = m - 1;
+        int nums2End = n - 1;
+        int insertPosition = nums1.length - 1;
 
+        while (nums2End >= 0) {
+            if (nums1End >= 0 && nums1[nums1End] > nums2[nums2End]) {
+                nums1[insertPosition--] = nums1[nums1End--];
+            } else{
+                nums1[insertPosition--] = nums2[nums2End--];
+            }
+        }
+        return nums1;
     }
 }
