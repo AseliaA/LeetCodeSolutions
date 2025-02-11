ecd361aa3c331bbe1425a8c5c377fbc037025573
AsiA203 <aselia.azimkanova@gmail.com>
2025-02-10 20:15:53 -0600

    - Watched recoding of the solution for  SortArrayByParity2_922_Easy.java

--- src/main/java/AlgorithmsAndDataStructure/week_14/SortArrayByParityII_922_Easy.java
+++ src/main/java/AlgorithmsAndDataStructure/week_14/SortArrayByParityII_922_Easy.java
@@ -1,11 +1,29 @@
 package AlgorithmsAndDataStructure.week_14;
 
+import java.util.Arrays;
+
 public class SortArrayByParityII_922_Easy {
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(sortArray(new int[]{4, 2, 5, 7})));
     }
 
     public static int[] sortArray(int [] nums) {
+        int evens = 0;
+        int odds = 1;
+        int n = nums.length;
 
+        while (evens < n && odds < n) {
+            if (/*evens < n && */nums[evens] % 2 == 0) evens += 2;
+            if (/*odds < n && */nums[odds] % 2 == 1) odds += 2;
+
+            if (evens < n && odds < n) swap(nums, evens, odds);
+        }
+        return nums;
+    }
+
+    public static void swap(int[] nums, int x, int y){
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
     }
 }
