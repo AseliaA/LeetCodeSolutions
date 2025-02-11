a92d7268eae2b723886e4bd0da26e558ef579908
AsiA203 <aselia.azimkanova@gmail.com>
2025-02-10 19:37:08 -0600

    - Started SortArrayByParity2_922_Easy.java

---
+++ src/main/java/AlgorithmsAndDataStructure/week_14/SortArrayByParityII_922_Easy.java
@@ -0,0 +1,11 @@
+package AlgorithmsAndDataStructure.week_14;
+
+public class SortArrayByParityII_922_Easy {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] sortArray(int [] nums) {
+
+    }
+}
