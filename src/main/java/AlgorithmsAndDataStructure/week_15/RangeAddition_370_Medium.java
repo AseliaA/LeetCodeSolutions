799fc6f4b0fef4208656f57f652f4ed3d35ab582
AsiA203 <aselia.azimkanova@gmail.com>
2025-02-22 16:26:30 -0600

    - Started solving RangeAddition_370_Medium.java

---
+++ src/main/java/AlgorithmsAndDataStructure/week_15/RangeAddition_370_Medium.java
@@ -0,0 +1,12 @@
+package AlgorithmsAndDataStructure.week_15;
+
+public class RangeAddition_370_Medium {
+    public static void main(String[] args) {
+
+    }
+
+    //ver1
+    public static int[] getModifiedArray(int length, int[][] updates) {
+
+    }
+}
