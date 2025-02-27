caff744e747fc1d390122e097b29c90933c4a923
AsiA203 <aselia.azimkanova@gmail.com>
2025-02-26 22:53:30 -0600

    - Implemented solution for GroupAnagrams_49_Medium.java

--- src/main/java/AlgorithmsAndDataStructure/week_16/GroupAnagrams_49.java
+++ src/main/java/AlgorithmsAndDataStructure/week_16/GroupAnagrams_49.java
@@ -1,4 +1,28 @@
 package AlgorithmsAndDataStructure.week_16;
 
+import java.util.*;
+
 public class GroupAnagrams_49 {
+    public static void main(String[] args) {
+        System.out.println(groupAnagrams(new String[]{"eat","tea","tan","ate","nat","bat"}));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) {
+            return new ArrayList<>();
+        }
+
+        Map<String, List<String>> map = new HashMap<>();
+        for (String word : strs) {
+            char [] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(word);
+        }
+        return new ArrayList<>(map.values());
+    }
 }
