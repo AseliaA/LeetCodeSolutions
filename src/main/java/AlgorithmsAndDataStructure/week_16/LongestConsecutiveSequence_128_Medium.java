8321076781e5fa7bb966a02b020498b94a1d4367
AsiA203 <aselia.azimkanova@gmail.com>
2025-02-26 22:55:30 -0600

    - Started solving LongestConsecutiveSequence_128_Medium.java

---
+++ src/main/java/AlgorithmsAndDataStructure/week_16/LongestConsecutiveSequence_128_Medium.java
@@ -0,0 +1,11 @@
+package AlgorithmsAndDataStructure.week_16;
+
+public class LongestConsecutiveSequence_128_Medium {
+    public static void main(String[] args) {
+
+    }
+
+    public static int longerConsecutiveSequence1(int [] nums){
+
+    }
+}
