7b3f6f74ccd05127c575c7c5b2f0ed52729369c7
AsiA203 <aselia.azimkanova@gmail.com>
2025-03-01 23:41:22 -0600

    - Implemented solution for LongestConsecutiveSequence_128_Medium.java

--- src/main/java/AlgorithmsAndDataStructure/week_16/LongestConsecutiveSequence_128_Medium.java
+++ src/main/java/AlgorithmsAndDataStructure/week_16/LongestConsecutiveSequence_128_Medium.java
@@ -1,11 +1,32 @@
 package AlgorithmsAndDataStructure.week_16;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LongestConsecutiveSequence_128_Medium {
     public static void main(String[] args) {
 
     }
 
     public static int longerConsecutiveSequence1(int [] nums){
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            set.add(n);
+        }
 
+        int longestStreak = 0;
+        for (int n : set) {
+            if (!set.contains(n - 1)) {
+                int streakStart = n;
+                int streakEnd = n;
+
+                while (set.contains(streakEnd + 1)) {
+                    streakEnd++;
+                }
+
+                longestStreak = Math.max(longestStreak, streakEnd - streakStart + 1);
+            }
+        }
+        return longestStreak;
     }
 }
