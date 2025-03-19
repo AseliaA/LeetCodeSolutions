c6f75c74039f95812916de25f3566cc40858da98
AsiA203 <aselia.azimkanova@gmail.com>
2025-03-18 23:28:38 -0500

    - Continuing with SubarraySumEqualsK_560_Medium

--- src/main/java/AlgorithmsAndDataStructure/week_16/SubarraySumEqualsK_560_Medium.java
+++ src/main/java/AlgorithmsAndDataStructure/week_16/SubarraySumEqualsK_560_Medium.java
@@ -1,7 +1,22 @@
 package AlgorithmsAndDataStructure.week_16;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SubarraySumEqualsK_560_Medium {
     public static void main(String[] args) {
 
+    }
+
+    public static int subarraySum(int[] nums, int k) {
+        int currentPrefixSum = 0;
+        int counter = 0;
+
+        Map<Integer, Integer> prefixSumSeenCount = new HashMap<>();
+        prefixSumSeenCount.put(0,1);
+
+        for (int i = 0; i < nums.length; i++) {
+            currentPrefixSum += nums[i];
+        }
     }
 }
