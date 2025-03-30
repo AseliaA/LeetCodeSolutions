479cef29938736146b1f675d7f6d3622db85ca55
AsiA203 <aselia.azimkanova@gmail.com>
2025-03-30 18:51:57 -0500

    - Implemented 2st solution for FindMinimumInRotatedSortedArray_153_Medium

--- src/main/java/AlgorithmsAndDataStructure/week_17/FindMinimumInRotatedSortedArray_153_Medium.java
+++ src/main/java/AlgorithmsAndDataStructure/week_17/FindMinimumInRotatedSortedArray_153_Medium.java
@@ -29,4 +29,26 @@ public class FindMinimumInRotatedSortedArray_153_Medium {
         }
         return answer;
     }
+
+    //teacher's version
+    public static int findMinTeachers(int[] nums) {
+        int n = nums.length;
+        if (nums == null || n == 0) return 0;
+        if (n == 1) return nums[0];
+        if (nums[0] < nums[n - 1]) return nums[0];
+
+        //continuing
+        int left = 0;
+        int right = n - 1;
+
+        while (left <= right) {
+            int middle = (left + right) / 2;
+
+            if (middle > 0 && nums[middle - 1] > nums[middle]) return nums[middle];
+
+            if (nums[middle] > nums[right]) left = middle + 1;
+            else right = middle - 1;
+        }
+        return -1;
+    }
 }
