95630a732da8287f5f4aca47aee62f82747eb574
AsiA203 <aselia.azimkanova@gmail.com>
2025-04-05 16:52:06 -0500

    - Solved on own and implemented teacher's solution of FibonacciNumber_509_Easy

--- src/main/java/AlgorithmsAndDataStructure/week_19/FibonacciNumber_509_Easy.java
+++ src/main/java/AlgorithmsAndDataStructure/week_19/FibonacciNumber_509_Easy.java
@@ -3,7 +3,29 @@ package AlgorithmsAndDataStructure.week_19;
 public class FibonacciNumber_509_Easy {
     public static void main(String[] args){}
 
-    public static int fib(int n) {
+    //Azret's solution
+    //time complexity: O(n)
+    //memory complexity: O(n)
+    int[] f;
 
+    private int fib_n(int n) {
+        if (n <= 1) return n;
+        if (f[n] == 0)
+            f[n] = fib_n(n - 1) + fib_n(n - 2);
+        return f[n];
+    }
+
+    public int fib(int n) {
+        f = new int[n + 1];
+        return fib_n(n);
+    }
+
+
+    //own solution
+    //time complexity: O(2^n)
+    //memory complexity: O(n)
+    public static int fib1(int n) {
+        if (n <= 0) return n;
+        return fib1(n - 1) + fib1(n - 2);
     }
 }
