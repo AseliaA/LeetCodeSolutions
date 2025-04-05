c420edce454e69c6c3daf9d9e00cb8fac50e8295
AsiA203 <aselia.azimkanova@gmail.com>
2025-04-05 14:56:30 -0500

    - Solved ContainerWithMostWater_11_Medium

--- src/main/java/AlgorithmsAndDataStructure/week_18/ContainerWithMostWater_11_Medium.java
+++ src/main/java/AlgorithmsAndDataStructure/week_18/ContainerWithMostWater_11_Medium.java
@@ -3,7 +3,27 @@ package AlgorithmsAndDataStructure.week_18;
 public class ContainerWithMostWater_11_Medium {
     public static void main(String[] args) {}
 
-    public static int maxArea(int[] heights) {
+    //own solution
+    //Time complexity: O(n)
+    //Memory complexity: O(1)
+    public static int maxArea1(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+//        int result = -1; *Evaluation: 0 is better, since area can't be negative
+//                         *result -> maxArea
+        int maxArea = 0;
 
+        while (left < right) {
+            int minHeight = Math.min(height[left], height[right]);
+//            int distance = Math.abs(right - left); *Math.abs() is not needed
+            int distance = right - left;
+            maxArea = Math.max(maxArea, minHeight * distance);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else right--;
+        }
+
+        return maxArea;
     }
 }
