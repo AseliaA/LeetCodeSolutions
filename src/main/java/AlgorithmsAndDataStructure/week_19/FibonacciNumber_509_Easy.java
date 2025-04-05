9afd6a8bd799b16a05e5ef0c6abf84f287dca223
AsiA203 <aselia.azimkanova@gmail.com>
2025-04-04 22:46:30 -0500

    - Started solving FibonacciNumber_509_Easy

---
+++ src/main/java/AlgorithmsAndDataStructure/week_19/FibonacciNumber_509_Easy.java
@@ -0,0 +1,9 @@
+package AlgorithmsAndDataStructure.week_19;
+
+public class FibonacciNumber_509_Easy {
+    public static void main(String[] args){}
+
+    public static int fib(int n) {
+
+    }
+}
