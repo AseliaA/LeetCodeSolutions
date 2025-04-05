f99a3a6fda751e9567f61dabb86213949af541b5
AsiA203 <aselia.azimkanova@gmail.com>
2025-04-04 22:44:40 -0500

    - Started solving ContainerWithMostWater_11_Medium

---
+++ src/main/java/AlgorithmsAndDataStructure/week_18/ContainerWithMostWater_11_Medium.java
@@ -0,0 +1,9 @@
+package AlgorithmsAndDataStructure.week_18;
+
+public class ContainerWithMostWater_11_Medium {
+    public static void main(String[] args) {}
+
+    public static int maxArea(int[] heights) {
+
+    }
+}
