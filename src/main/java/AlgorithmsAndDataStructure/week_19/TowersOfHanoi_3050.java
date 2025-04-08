4759efe762ca47ecb7cfd078ed2e3c7079f1cd9e
AsiA203 <aselia.azimkanova@gmail.com>
2025-04-07 20:55:36 -0500

    - Implemented solution for TowersOfHanoi_3050

--- src/main/java/AlgorithmsAndDataStructure/week_19/TowersOfHanoi_3050.java
+++ src/main/java/AlgorithmsAndDataStructure/week_19/TowersOfHanoi_3050.java
@@ -1,5 +1,19 @@
 package AlgorithmsAndDataStructure.week_19;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TowersOfHanoi_3050 {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        hanoi(3, 1, 3);
+    }
+
+    public static void hanoi(int n, int from, int to) {
+        if (n == 0) return;
+
+        int aux = (1 + 2 + 3) - (from + to);
+        hanoi(n - 1, from, aux);
+        System.out.println(n + " " + from + " " + to);
+        hanoi(n - 1, aux, to);
+    }
 }
