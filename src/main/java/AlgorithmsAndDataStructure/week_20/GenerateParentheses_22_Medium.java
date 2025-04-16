f367a46e858a19079ab815a9f87fa375b807fa41
AsiA203 <aselia.azimkanova@gmail.com>
2025-04-15 21:27:31 -0500

    - Implemented solution for GenerateParentheses_22_Medium

--- src/main/java/AlgorithmsAndDataStructure/week_20/GenerateParentheses_22_Medium.java
+++ src/main/java/AlgorithmsAndDataStructure/week_20/GenerateParentheses_22_Medium.java
@@ -1,7 +1,31 @@
 package AlgorithmsAndDataStructure.week_20;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenerateParentheses_22_Medium {
     public static void main(String[] args) {
 
+    }
+
+    public static List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<String>();
+        recurse(res, 0, 0, "", n);
+        return res;
+    }
+
+    public static void recurse(List<String> res, int left, int right, String s, int n) {
+        if (s.length() == n * 2) {
+            res.add(s);
+            return;
+        }
+
+        if (left < n) {
+            recurse(res, left + 1, right, s + "(", n);
+        }
+
+        if (right < left) {
+            recurse(res, left, right + 1, s + ")", n);
+        }
     }
 }
