7ba51d0da80accbb5bbb146dcee0ebcaeafee142
AsiA203 <aselia.azimkanova@gmail.com>
2025-04-16 20:49:13 -0500

    - Started solving SudokuSolver_37_Hard

---
+++ src/main/java/AlgorithmsAndDataStructure/week_20/SudokuSolver_37_Hard.java
@@ -0,0 +1,11 @@
+package AlgorithmsAndDataStructure.week_20;
+
+public class SudokuSolver_37_Hard {
+    public static void main(String[] args) {
+
+    }
+
+    public static void solveSudoku(char[][] board) {
+
+    }
+}
