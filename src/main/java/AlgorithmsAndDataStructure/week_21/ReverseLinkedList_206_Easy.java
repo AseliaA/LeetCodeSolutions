7fe909bf0d6425698d074181b7228f7b558e797d
AsiA203 <aselia.azimkanova@gmail.com>
2025-05-06 22:31:45 -0500

    - Started solving ReverseLinkedList_206_Easy

---
+++ src/main/java/AlgorithmsAndDataStructure/week_21/ReverseLinkedList_206_Easy.java
@@ -0,0 +1,13 @@
+package AlgorithmsAndDataStructure.week_21;
+
+public class ReverseLinkedList_206_Easy {
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        
+    }
+}
+
+
