f84da91494f92262167113cfca259bde48f7c343
AsiA203 <aselia.azimkanova@gmail.com>
2025-05-08 17:18:13 -0500

    - Implemented solution for ReverseLinkedList_206_Easy

--- src/main/java/AlgorithmsAndDataStructure/week_21/ReverseLinkedList_206_Easy.java
+++ src/main/java/AlgorithmsAndDataStructure/week_21/ReverseLinkedList_206_Easy.java
@@ -6,7 +6,14 @@ public class ReverseLinkedList_206_Easy {
     }
 
     public static ListNode reverseList(ListNode head) {
-        
+        return reverseListRecursive(head, null);
+    }
+
+    private static ListNode reverseListRecursive(ListNode currentNode, ListNode newNext) {
+        if (currentNode == null) return newNext;
+        ListNode oldNode = currentNode.next;
+        currentNode.next = newNext;
+        return reverseListRecursive(oldNode, newNext);
     }
 }
 
