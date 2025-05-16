37fdccec22577f5d7327d2bcc347eda477226e42
AsiA203 <aselia.azimkanova@gmail.com>
2025-05-16 14:22:46 -0500

    - Implemented optimized solution for AddTwoNumbers_2_Medium

--- src/main/java/AlgorithmsAndDataStructure/week_21/AddTwoNumbers_2_Medium.java
+++ src/main/java/AlgorithmsAndDataStructure/week_21/AddTwoNumbers_2_Medium.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 
 public class AddTwoNumbers_2_Medium {
     public static void main(String[] args) {
+        //own (not good) implementation
         ListNode l1 = new ListNode(2);
         l1.next = new ListNode(4);
         l1.next.next = new ListNode(3);
@@ -12,14 +13,48 @@ public class AddTwoNumbers_2_Medium {
         l2.next = new ListNode(6);
         l2.next.next = new ListNode(4);
 
-        ListNode head = addTwoNumbers(l1, l2);
+        ListNode head = addTwoNumbers1(l1, l2);
         while (head != null) {
             System.out.println(head.val);
             head = head.next;
         }
+
+        //optimized solution
+        ListNode head1 = addTwoNumbers(l1, l2);
+        while (head1 != null) {
+            System.out.println(head1.val);
+            head1 = head1.next;
+        }
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        int carryOver = 0;
+
+        while (l1 != null || l2 != null || carryOver == 1) {
+            int sum = 0;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            sum += carryOver;
+            carryOver = sum / 10;
+
+            tail.next = new ListNode(sum % 10);
+            tail = tail.next;
+        }
+
+        return dummy.next;
+    }
+
+    public static ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
         if (l1 == null || l1.val == 0) {
             return l2;
         } else if (l2 == null || l2.val == 0) {
