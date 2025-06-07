a621a5655e257d8197670caae471bd51172b2e9a
AsiA203 <aselia.azimkanova@gmail.com>
2025-06-06 19:38:40 -0500

    - Implemented solution for ImplementQueueUsingStacks_232_Easy.java

---
+++ src/main/java/AlgorithmsAndDataStructure/week_23/ImplementQueueUsingStacks_232_Easy.java
@@ -0,0 +1,33 @@
+package AlgorithmsAndDataStructure.week_23;
+
+import java.util.Stack;
+
+public class ImplementQueueUsingStacks_232_Easy {
+    static private final Stack<Integer> input = new Stack<>();
+    static private final Stack<Integer> output = new Stack<>();
+
+    public static void MyQueue(String[] args) {}
+
+    public static void push(int x) {
+        input.push(x);
+    }
+
+    public static int pop() {
+        int x = peek();
+        output.pop();
+        return x;
+    }
+
+    public static int peek() {
+        if (output.isEmpty()) {
+            while (!input.isEmpty()) {
+                output.push(input.pop());
+            }
+        }
+        return output.peek();
+    }
+
+    public static boolean isEmpty() {
+        return input.isEmpty() && output.isEmpty();
+    }
+}
