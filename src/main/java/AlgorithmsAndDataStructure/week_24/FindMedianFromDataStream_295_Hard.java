a51fc5ce6f2c4e2495ca4dc8af79cbec2bc3f400
AsiA203 <aselia.azimkanova@gmail.com>
2025-07-10 19:48:44 -0500

    - Implemented solution for FindMedianFromDataStream_295_Hard

--- src/main/java/AlgorithmsAndDataStructure/week_24/FindMedianFromDataStream_295_Hard.java
+++ src/main/java/AlgorithmsAndDataStructure/week_24/FindMedianFromDataStream_295_Hard.java
@@ -1,6 +1,28 @@
 package AlgorithmsAndDataStructure.week_24;
 
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 //2
 public class FindMedianFromDataStream_295_Hard {
-    public static void main(String[] args) {}
+    private PriorityQueue<Integer> maxHeap = new PriorityQueue<>();
+    private PriorityQueue<Integer> minHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+    public static void MedianFinder(String[] args) {}
+
+    public void addNum(int num) {
+        maxHeap.offer(num);
+        minHeap.offer(maxHeap.poll());
+
+        if (minHeap.size() > maxHeap.size()) {
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (maxHeap.size() > minHeap.size()) {
+            return maxHeap.peek();
+        }
+        return (minHeap.peek() + maxHeap.peek()) / 2.0d;
+    }
 }
