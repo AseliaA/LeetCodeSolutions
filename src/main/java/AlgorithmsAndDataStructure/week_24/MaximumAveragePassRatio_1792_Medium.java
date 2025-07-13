e6948084e126b9101e91e75a45d04eac6712b934
AsiA203 <aselia.azimkanova@gmail.com>
2025-07-13 16:08:09 -0500

    - Implemented solution for MaximumAveragePassRatio_1792_Medium

--- src/main/java/AlgorithmsAndDataStructure/week_24/MaximumAveragePassRatio_1792_Medium.java
+++ src/main/java/AlgorithmsAndDataStructure/week_24/MaximumAveragePassRatio_1792_Medium.java
@@ -1,5 +1,38 @@
 package AlgorithmsAndDataStructure.week_24;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class MaximumAveragePassRatio_1792_Medium {
     public static void main(String[] args) {}
+
+    public static double maxAverageRatio(int[][] classes, int extraStudents) {
+        PriorityQueue<double[]> maxPassRatioHeap = new PriorityQueue<>(
+                Comparator.comparingDouble(o -> -o[0])
+        );
+
+        for (int[] c : classes) {
+            double p = c[0], t = c[1];
+            maxPassRatioHeap.offer(new double[]{maxPassRatio(p, t), p, t});
+        }
+
+        while (extraStudents > 0) {
+            double[] top = maxPassRatioHeap.poll();
+            double p = top[1], t = top[2];
+            maxPassRatioHeap.offer(new double[]{maxPassRatio(p + 1, t + 1), p + 1, t + 1});
+            extraStudents--;
+        }
+
+        double answer = 0.0;
+        while (!maxPassRatioHeap.isEmpty()) {
+            double[] top = maxPassRatioHeap.poll();
+            double p = top[1], t = top[2];
+            answer += p / t;
+        }
+        return answer / classes.length;
+    }
+
+    public static double maxPassRatio(double pass, double total) {
+        return (pass + 1) / (total + 1) - pass/total;
+    }
 }
