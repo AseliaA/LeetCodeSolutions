4b60fd50d51134f2c50b0c767c2e237ec8b8009a
AsiA203 <aselia.azimkanova@gmail.com>
2025-07-14 20:53:06 -0500

    - Implemented solution for TopKFrequentWords_692_Medium

--- src/main/java/AlgorithmsAndDataStructure/week_24/TopKFrequentWords_692_Medium.java
+++ src/main/java/AlgorithmsAndDataStructure/week_24/TopKFrequentWords_692_Medium.java
@@ -1,4 +1,40 @@
 package AlgorithmsAndDataStructure.week_24;
 
+import java.util.*;
+
 public class TopKFrequentWords_692_Medium {
+    public static void main(String[] args) {
+    }
+
+    public static List<String> topKFrequent(String[] words, int k) {
+        List<String> result = new LinkedList<>();
+
+        Map<String, Integer> freqMap = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            if (freqMap.containsKey(words[i])) {
+                freqMap.put(words[i], freqMap.get(words[i]) + 1);
+            } else {
+                freqMap.put(words[i], 1);
+            }
+        }
+
+        PriorityQueue<Map.Entry<String, Integer>> freqHeap = new PriorityQueue<>(
+                (a, b) -> (a.getValue() == b.getValue() ? // if freq equals
+                        b.getKey().compareTo(a.getKey()) : //put lexicographical larger first
+                        a.getValue() - b.getValue()) //otherwise, the least freq to the front
+        );
+
+        for (Map.Entry<String, Integer> entry : freqMap.entrySet()) {
+            freqHeap.offer(entry);
+
+            if (freqHeap.size() == k + 1) {
+                freqHeap.poll();
+            }
+        }
+
+        while (!freqHeap.isEmpty()) {
+            result.add(0, freqHeap.poll().getKey());
+        }
+        return result;
+    }
 }
